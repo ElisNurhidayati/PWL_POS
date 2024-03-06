98b81cec34eaf15cdf70303e6b6cac105dc82419
Elis Nurhidayati <elisnurhidayati777@gmail.com>
2024-03-06 10:13:17 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,17 +11,21 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data); //tambah data  m_users
-
-        //Akses model UserModel
-        $user = UserModel::all();  //Mengambil semua data tabel m_users
+        $user = UserModel::findOr(20, ['usename', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
 
+// $data = [
+//     'level_id' => 2,
+//     'username' => 'manager_tiga',
+//     'nama' => 'Manager 3',
+//     'password' => Hash::make('12345')
+// ];
+// UserModel::create($data); //tambah data  m_users
+
+// //Akses model UserModel
+// $user = UserModel::all();  //Mengambil semua data tabel m_users
+// return view('user', ['data' => $user]);
