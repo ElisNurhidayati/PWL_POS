c2c0b7759da7386ab5b0057ac7072cb448837635
Elis Nurhidayati <elisnurhidayati777@gmail.com>
2024-03-11 01:14:55 +0700

    Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,31 +8,48 @@ use Illuminate\Support\Facades\Hash;
 // use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-// class UserController extends Controller
-// {
-//     public function index()
-//     {
-//         $users = UserModel::where('level_id', 2)->get(); // Use get() instead of count() to retrieve the data
-//         $userCount = $users->count(); // Get the count of users
-//         return view('user', ['data' => $users, 'userCount' => $userCount]); // Pass the users data and count to the view
-//     }
-// }
-
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew([
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
-
     }
 }
+
+// $user = UserModel::firstOrCreate([
+//     'username' => 'manager22',
+//     'nama' => 'Manager Dua Dua',
+// ],
+// );
+// return view('user', ['data' => $user]);
+
+
+// $user = UserModel::firstOrCreate([
+//     'username' => 'manager22',
+//     'nama' => 'Manager Dua Dua',
+//     'password' => Hash::make('12345'),
+//     'level_id' => 2
+// ],
+// );
+// return view('user', ['data' => $user]);
+
+// $user = UserModel::where('level_id', 2)->count();
+// dd($user);
+// return view('user', ['data' => $user]);
 
 
 // $user = UserModel::where('username', 'manager9')->firstOrFail();
         // return view('user', ['data' => $user]);
+
 
 // $user = UserModel::fidOrFail(20, ['usename', 'nama'], function() {
 //     abort(404);
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,11 +1,10 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Data User</title>
 </head>
 <body>
     <h1>Data User</h1>
-    <p>Jumlah Pengguna: {{$user}} </p>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
@@ -13,20 +12,18 @@
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
-        @foreach ($data as $dd)
             <tr>
-                <td>{{$d->user_id}}</td>
-                <td>{{$d->username}}</td>
-                <td>{{$d->nama}}</td>
-                <td>{{$d->level_id}}</td>
+                <td>{{$data->user_id}}</td>
+                <td>{{$data->username}}</td>
+                <td>{{$data->nama}}</td>
+                <td>{{$data->level_id}}</td>
             </tr>
-        @endforeach
     </table>
 </body>
-</html> --}}
+</html>
 
 
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Data User</title>
@@ -42,4 +39,4 @@
             </tr>
     </table>
 </body>
-</html>
+</html> --}}
