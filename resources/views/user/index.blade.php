033e0220dc702fd1f07f750eb20e9d8a7a98303a
Elis Nurhidayati <elisnurhidayati777@gmail.com>
2024-04-16 19:10:42 +0700

    JQuery Laravel datatable P7

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,24 +1,3 @@
-{{-- @extends('layouts.app') --}}
-{{-- Customize layout sections --}}
-{{-- @section('subtitle', 'User')
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'User')
-@section('content')
-    <div class="container">
-        <div class="card">
-            <div class="card-header">Manage User</div>
-            <div class="card-body">
-                <a href="{{ route('/user/create') }}" class="btn btn-primary mb-2">+ Add User</a>
-                {{ $dataTable->table() }}
-            </div>
-        </div>
-    </div>
-@endsection
-@push('scripts')
-    {{ $dataTable->scripts() }}
-@endpush --}}
-
-{{-- P7 --}}
 @extends('layouts.template') 
  
 @section('content') 
