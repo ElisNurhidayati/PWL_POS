6ea8a1eb9e7ae7459a078ef1ff22712709e01d30
Elis Nurhidayati <elisnurhidayati777@gmail.com>
2024-05-14 03:33:56 +0700

    Jobsheet 10 RESTful logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,8 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
-});
+    });
+    Route::post('/logout', LogoutController::class)->name('logout');
 });
 
 
