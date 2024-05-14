e48921a6eaaee4f5fa0ef28befbe362349b40755
Elis Nurhidayati <elisnurhidayati777@gmail.com>
2024-05-14 09:37:30 +0700

    Jobsheet 10 RESTful CRUD levels

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,12 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/logout', LogoutController::class)->name('logout');
 });
 
+//CRUD LEVEL
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
 /*
 |--------------------------------------------------------------------------
